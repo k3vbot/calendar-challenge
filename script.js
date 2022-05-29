28958dc79b9094868d6f6d7e97216ba786c2b65a
Kevin <muehl004@gmail.com>
2022-05-28 20:49:58 -0700

    started timeblocks

---
+++ script.js
@@ -0,0 +1,8 @@
+const todaysDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
+$("#currentDay").text(todaysDate);
+
+const timeBlocks = document.getElementsByClassName("container")
+
+function calendar() {
+
+}
