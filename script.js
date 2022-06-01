1ef528ca5b98c482948fb38cf71e7c1b15ccccdd
Kevin <muehl004@gmail.com>
2022-06-01 15:51:58 -0700

    added save button and pseudo code for calendar function

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const todaysDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
 $("#currentDay").text(todaysDate);
 
 const timeBlocks = document.getElementsByClassName("container")
+//Build out calendar function
+//check for current time, and highlight the corresponding timeblock with present color
+//highlight past and future timeblocks with corresponding colors
 
 function calendar() {
 
+}
+document.querySelector('.saveBtn').onclick = function(event) {
+    const save = event.target.innerHTML;
+    console.log(save);
 }
